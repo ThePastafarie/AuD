89b054f60148a09434040849fdeeaa32d91bca77
ThePastafarie <peterwerber1406@googlemail.com>
2021-04-15 08:44:30 +0200

    Anfang 0,1

---
+++ Blatt_0_Weber/Aufgabe1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ggTeiler (int a, int b);
+
+int main (void){
+
+    int a = NULL;
+    int b = NULL;
+
+
+
+}
+
+int ggTeiler (int a, int b){
+    if (b =! 0){
+
+    }else{
+        
+    }
+}
